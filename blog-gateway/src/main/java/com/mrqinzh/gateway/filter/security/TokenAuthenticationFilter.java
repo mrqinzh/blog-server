4e7d66f86ee37085785baa2104028617cb22d6fe
zhihong.qin 秦志宏 <zhihong.qin@qiyuesuo.com>
2024-08-27 19:05:27 +0800

    调整docker文件

--- blog-gateway/src/main/java/com/mrqinzh/gateway/filter/security/TokenAuthenticationFilter.java
+++ blog-gateway/src/main/java/com/mrqinzh/gateway/filter/security/TokenAuthenticationFilter.java
@@ -1,10 +1,8 @@
 package com.mrqinzh.gateway.filter.security;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
