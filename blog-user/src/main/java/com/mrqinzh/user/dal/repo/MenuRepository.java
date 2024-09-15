2b1d98e3806845c3b369ed6e410c9cb5c7555ae0
zhihong.qin 秦志宏 <zhihong.qin@qiyuesuo.com>
2024-09-15 23:39:02 +0800

    fix

--- blog-user/src/main/java/com/mrqinzh/user/dal/repo/MenuRepository.java
+++ blog-user/src/main/java/com/mrqinzh/user/dal/repo/MenuRepository.java
@@ -1,7 +1,13 @@
 package com.mrqinzh.user.dal.repo;
 
+import com.mrqinzh.user.dal.mapper.MenuMapper;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MenuRepository {
+
+    @Resource
+    private MenuMapper menuMapper;
+
 }
--- blog-user/src/main/java/com/mrqinzh/user/dal/repo/RoleRepository.java
+++ blog-user/src/main/java/com/mrqinzh/user/dal/repo/RoleRepository.java
@@ -1,7 +1,13 @@
 package com.mrqinzh.user.dal.repo;
 
+import com.mrqinzh.user.dal.mapper.RoleMapper;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RoleRepository {
+
+    @Resource
+    private RoleMapper roleMapper;
+
 }
